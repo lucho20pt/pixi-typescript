af2f6091f197130574a15c4a05b8bb1a0e20ce40
Daniel Batista <dani_batista20@hotmail.com>
2022-11-03 22:12:56 +0000

    add: styles.ts to splite code styles related

---
+++ src/styles.ts
@@ -0,0 +1,21 @@
+export const pIndex = {
+    fill: 0x212121,
+    fontFamily: "Trebushet MS",
+    fontSize: 16,
+    wordWrap: true,
+    wordWrapWidth: 0,
+};
+export const pName = {
+    fill: 0x212121,
+    fontFamily: "Trebushet MS",
+    fontSize: 16,
+    wordWrap: true,
+    wordWrapWidth: 100,
+};
+export const pScore = {
+    fill: 0x212121,
+    fontFamily: "Trebushet MS",
+    fontSize: 14,
+    wordWrap: true,
+    wordWrapWidth: 100,
+};
