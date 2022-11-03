e040adb79784f4bdada38c7d6e08b560013d9c6b
Daniel Batista <dani_batista20@hotmail.com>
2022-11-03 22:13:29 +0000

    add: loop trougth async data endpoints add: playerContainer addChilds index, name, score

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import { Application, Loader, Text } from "pixi.js";
+import { Application, Loader, Text, Container } from "pixi.js";
+import { pIndex, pName, pScore } from "./styles";
 import "./style.css";
 
 // declare const VERSION: string;
 // console.log(`Welcome from pixi-typescript-boilerplate ${VERSION}`);
 
-const urlPlayers =
-    "https://testing.cdn.arkadiumhosted.com/gameExamples/programming-assignments/senior-core-developer/players.json";
-const urlQuotes =
-    "https://testing.cdn.arkadiumhosted.com/gameExamples/programming-assignments/senior-core-developer/quotes.json";
+const urlPlayers = "./assets/data/players.json";
+const urlQuotes = "./assets/data/players.json";
+// const urlPlayers = "https://testing.cdn.arkadiumhosted.com/gameExamples/programming-assignments/senior-core-developer/players.json";
+// const urlQuotes = "https://testing.cdn.arkadiumhosted.com/gameExamples/programming-assignments/senior-core-developer/quotes.json";
+// const urlAvatars = "https://testing.cdn.arkadiumhosted.com/gameExamples/programming-assignments/senior-core-developer/avatars/2/8.png";
 
-const gameWidth = 800;
+// CANVAS
+const gameWidth = 960;
 const gameHeight = 600;
 
 const app = new Application({
-    backgroundColor: 0xf2f2f2,
+    backgroundColor: 0x00bbbb,
     width: gameWidth,
     height: gameHeight,
 });
@@ -31,32 +34,53 @@ const fetchPlayersAndQuotes = async () => {
     const quotes = await quotesResponse.json();
     return [players, quotes];
 };
+
 fetchPlayersAndQuotes()
     .then(([players, quotes]) => {
-        console.log("players =>", players);
-        // console.log("quotes =>", quotes);
         players; // fetched players
         quotes; // fetched quotes
+        // console.log("quotes =>", quotes);
+        console.log("players =>", players["players"]);
+
+        // variables
+        let y = 0;
+        // const playerLen = players["players"].length;
+
+        for (let i = 0; i < 5; i++) {
+            y += 40;
+            const playerContainer = new Container();
+            //
+            const playerIndex: number = players["players"][i].index;
+            const playerName: string = players["players"][i].name;
+            const playerScore: number = players["players"][i].score;
+            //
+            const index = new Text(playerIndex, pIndex);
+            const name = new Text(playerName, pName);
+            const score = new Text(playerScore, pScore);
+            //
+            index.position.set(100, 0);
+            name.position.set(100, 0);
+            score.position.set(200, 0);
+
+            // app.stage.addChild(player);
+            playerContainer.addChild(index).addChild(name).addChild(score);
+            playerContainer.position.set(10, y);
+            app.stage.addChild(playerContainer);
+            // player.anchor.set(0.5, 0.5);
+        }
     })
     .catch((error) => {
         // /players or /quotes request failed
         console.log("error =>", error.message);
     });
 
-// BASIC TEXT
-const basicText = new Text("Hello World");
-basicText.anchor.set(0.5, 0.5);
-basicText.position.set(gameWidth / 2, 50);
-
 // ON LOAD
 window.onload = async (): Promise<void> => {
     await loadGameAssets();
 
     document.body.appendChild(app.view);
-    app.stage.addChild(basicText);
-    app.stage.addChild(basicText);
 
-    resizeCanvas();
+    // resizeCanvas();
     app.stage.interactive = true;
 };
 
